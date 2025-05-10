24d918ad180ea866866148a92d3f4e1cb6506cfd
RomanDruzhba <98876439+RomanDruzhba@users.noreply.github.com>
2025-05-10 21:35:59 +0700

    improvements2

--- server/models/Genre.js
+++ server/models/Genre.js
@@ -4,9 +4,9 @@ import { db } from '../db.js';
 export class Genre extends Model {}
 
 Genre.init({
-    name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-    },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
 }, { sequelize: db, modelName: 'genre' });
--- server/models/Tag.js
+++ server/models/Tag.js
@@ -4,9 +4,9 @@ import { db } from '../db.js';
 export class Tag extends Model {}
 
 Tag.init({
-    name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-    },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
 }, { sequelize: db, modelName: 'tag' });
