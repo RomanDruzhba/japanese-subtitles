cb3619b589bc2277e695740c507ae55218627c95
RomanDruzhba <98876439+RomanDruzhba@users.noreply.github.com>
2025-05-10 23:49:57 +0700

    server6

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ const __dirname = path.dirname(__filename);
 
 
 const app = express();
-const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
+const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 5627;
 
 app.use(session({
   secret: 'secret-key',
